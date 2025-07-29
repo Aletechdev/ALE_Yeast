85753a0ac1988d6db30c826d382c6f133377e813
Zhiwei Li <zhli@dtu.dk>
2025-07-29 16:20:27 +0200

    quick save
    TODO: check if can still run 3.5.1, there should be some docker file / folder permission issue with SNPeff db input

--- bin/CENPK_run_sarek.sh
+++ bin/CENPK_run_sarek.sh
@@ -1,7 +1,7 @@
 run_folder="/Users/zhlia/Documents/GitRepo/NF_ALE"
-nextflow run nf-core/sarek -r 3.4.0 -profile docker \
-    --input ${run_folder}/data/data_a_paper/samplesheet_m3.csv \
-    --outdir ${run_folder}/output_a_paper_noMutect  --genome draft_ref.52 --igenomes_ignore \
+nextflow run ../nf-core-sarek_3.4.0/3_4_0/main.nf -profile docker \
+    --input ${run_folder}/data/data_a_paper/sub_sample/samplesheet.csv \
+    --outdir ${run_folder}/output  --genome draft_ref.52 --igenomes_ignore \
     --fasta ${run_folder}/data/BakerYeast_reference/draft_ref52.fasta --skip_tools baserecalibrator \
-    -c ${run_folder}/bin/nextflow.config --tools freebayes,manta,snpeff --split_fastq 50000000  \
-    --snpeff_cache ${run_folder}/data/BakerYeast_reference/snpeff_cache --snpeff_db draft_ref.52 -resume
+    -c ${run_folder}/bin/nextflow.config --tools freebayes,manta,snpeff --split_fastq 0  \
+    --snpeff_cache ${run_folder}/data/BakerYeast_reference/snpeff_cache --snpeff_db draft_ref.52 #-resume
