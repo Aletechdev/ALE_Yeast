18a32eaad2743954a60105e55227bf6d992488de
Zhiwei Li <zhli@dtu.dk>
2025-07-30 15:33:05 +0200

    clean up names

--- bin/CENPK_run_sarek.sh
+++
@@ -1,7 +0,0 @@
-run_folder="/Users/zhlia/Documents/GitRepo/NF_ALE"
-nextflow run ../nf-core-sarek_3.4.0/3_4_0/main.nf -profile arm,docker \
-    --input ${run_folder}/data/data_a_paper/sub_sample/samplesheet.csv \
-    --outdir ${run_folder}/output_34  --genome draft_ref.52 --igenomes_ignore \
-    --fasta ${run_folder}/data/BakerYeast_reference/draft_ref52.fasta --skip_tools baserecalibrator \
-    -c ${run_folder}/bin/nextflow.config --tools freebayes,snpeff --split_fastq 0  \
-    --snpeff_cache ${run_folder}/data/BakerYeast_reference/snpeff_cache --snpeff_db draft_ref.52 -resume
