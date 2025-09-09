9efdf1f039498f537985ed05ccb5266d4764fa1b
Zhiwei Li <zhlia@dtu.dk>
2025-09-09 11:39:00 +0000

    new work folder

--- bin/CENPK_run_sarek_351_all.sh
+++ bin/CENPK_run_sarek_351_all.sh
@@ -1,6 +1,7 @@
 # run_folder="/Users/zhlia/Documents/GitRepo/NF_ALE"
 run_folder="/home/azureuser/Docs/ALE_nextflow"  #--joint_germline \     --concatenate_vcfs \
 nextflow run ../nf-core-sarek_3.5.1/3_5_1/main.nf -profile azureD4as,docker \
+    -w ${run_folder}/work_CENPK \
     --input ${run_folder}/data/data_a_paper/samplesheet.csv \
     --outdir ${run_folder}/output_all  --genome null --igenomes_ignore  \
     --fasta ${run_folder}/data/BakerYeast_reference/draft_ref52.fasta --skip_tools baserecalibrator \
