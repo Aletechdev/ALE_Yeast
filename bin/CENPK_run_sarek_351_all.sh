9bd465c9dc638bb1788447e59a4cd259ca4bddd7
Zhiwei Li <zhlia@dtu.dk>
2025-10-13 17:00:22 +0200

    more samples, from spore analysis

--- bin/CENPK_run_sarek_351_all.sh
+++ bin/CENPK_run_sarek_351_all.sh
@@ -2,7 +2,7 @@
 run_folder="/home/azureuser/Docs/ALE_nextflow"  #--joint_germline \     --concatenate_vcfs \
 nextflow run ../nf-core-sarek_3.5.1/3_5_1/main.nf -profile azureD4as,docker \
     -w ${run_folder}/work_CENPK \
-    --input ${run_folder}/data/data_a_paper/samplesheet.csv \
+    --input ${run_folder}/data/data_a_paper/samplesheet_gen2.csv \
     --outdir ${run_folder}/output_all  --genome null --igenomes_ignore  \
     --fasta ${run_folder}/data/BakerYeast_reference/draft_ref52.fasta --skip_tools baserecalibrator \
     -c ${run_folder}/bin/nextflow.config --tools snpeff,freebayes,controlfreec,manta,mutect2,cnvkit,msisensorpro,tiddit,haplotypecaller,deepvariant --split_fastq 0  \
